41093d5021fd338924175dbac06222a88ba7fd15
Nefrit Mahardika <nefritmhd@gmail.com>
2021-10-03 15:30:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector('.hamburger input');
+const nav = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', function (){
+    nav.classList.toggle('slide');
+});
+
+$(".step").click( function() {
+	$(this).addClass("active").prevAll().addClass("active");
+	$(this).nextAll().removeClass("active");
+});
+
+$(".step01").click( function() {
+	$("#line-progress").css("width", "3%");
+	$(".discovery").addClass("active").siblings().removeClass("active");
+});
+
+$(".step02").click( function() {
+	$("#line-progress").css("width", "25%");
+	$(".strategy").addClass("active").siblings().removeClass("active");
+});
+
+$(".step03").click( function() {
+	$("#line-progress").css("width", "50%");
+	$(".creative").addClass("active").siblings().removeClass("active");
+});
+
+$(".step04").click( function() {
+	$("#line-progress").css("width", "75%");
+	$(".production").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click( function() {
+	$("#line-progress").css("width", "100%");
+	$(".analysis").addClass("active").siblings().removeClass("active");
+});
+
